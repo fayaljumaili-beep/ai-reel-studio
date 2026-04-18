bf915450d17e07229c28138f25d61d9fc3806362
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-04-18 22:24:58 +0100

    fix video request body script

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ audioUrl }),
+        body: JSON.stringify({ script }),
       });
 
       const data = await res.json();
